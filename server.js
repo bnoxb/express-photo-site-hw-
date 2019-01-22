8f6b3baff983233682bb90bec32b9bbdf702c96a
Robbie <saucy.cooking@gmail.com>
2019-01-22 14:27:07 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+require('db/db');
+const express   = require('express');
+const app       = express();
+const port      = 3000;
+const userController = require('./controllers/userController');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+
+app.use(bodyParser.urlencoded(extended: false));
+app.use(methodOverride('_method'));
+app.use('/users', userController);
+
+app.listen(port, ()=>{
+    console.log(`server is listening on port: ${port}`);
+})
